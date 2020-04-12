187bf14f0b6e171c5bc6038d81eda894cba7710f
codingclassmates <63534955+codingclassmates@users.noreply.github.com>
2020-04-12 17:26:03 -0400

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // library
+
+int main() // every program needs a "main" function
+{
+    printf("Hello World\n"); // print out "Hello World" followed by a newline, to the console (terminal)
+
+    return 0; // every main program needs to return 0 for indication of a successful run
+}
